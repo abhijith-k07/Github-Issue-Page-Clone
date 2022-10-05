269761fd7bd55a60bc2b576c95494e5735df9963
abhijith-k07 <88900086+abhijith-k07@users.noreply.github.com>
2022-10-05 06:46:18 +0530

    fix - issue creation

--- Backend/services/models/issue.model.js
+++ Backend/services/models/issue.model.js
@@ -4,12 +4,20 @@ class Issue {
     constructor({title, description, labels, raisedBy}) {
         this.title = title;
         this.description = description;
-        this.labels = labels ? labels : [];
+        this.labels = labels ? this.formatLabels(labels) : this.formatLabels([]);
         this.id = uuidv4();
         this.raisedBy = raisedBy;
         this.issueNumber = 1;
         this.status = 'open';
         this.issueTypeId = uuidv4();
+    }
+
+    
+    formatLabels(labels) {
+        if (labels.length === 0) return '';
+        return labels.reduce((prev, current) => {
+            return prev + ';' + current; 
+        })
     }
 }
 
